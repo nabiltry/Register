379a3324596ece78ae6238904db1ecb46d40683c
nabiltry <144593299+nabiltry@users.noreply.github.com>
2023-12-17 18:00:33 +0700

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 // middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
-const {getToken} = require ('../getToken'); 
+const { getToken } = require('../getToken');
 const config = require('../config/config');
 
 module.exports = (req, res, next) => {
@@ -8,14 +8,16 @@ module.exports = (req, res, next) => {
         const token = getToken(req)
         if (!token) return res.status(401).json({ error: true, message: 'authenticate faileds' })
 
-        const verifyToken = jwt.verifyToken(token)
+        const verifyToken = jwt.verify(token, config.jwtSecret)
         if (!verifyToken) return res.status(401).json({ error: true, message: 'authenticate faileds' })
 
-        req.user = verifyToken
+        req.users = verifyToken
         next()
 
     } catch (error) {
         next(error)
     }
 }
+
+
 
